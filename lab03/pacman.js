ee8b0e7207535efbf030c4ee7693a73e0635c14b
justinwang <justinwang407@gmail.com>
2024-05-31 15:20:46 -0400

    Added left and right movement

--- lab03/pacman.js
+++ lab03/pacman.js
@@ -11,12 +11,49 @@ function createGame(n) {
     }
 
     board[half] = pacman;
-    board[Math.floor(Math.random() * 11)] = fruit;
-    board[Math.floor(Math.random() * 11)] = ghost;
+    board[Math.floor(Math.random() * (n + 1))] = fruit;
+    board[Math.floor(Math.random() * (n + 1))] = ghost;
 
     console.log(board);
     return board;
 
 }
 
-createGame(10);
+function moveLeft(game) {
+    let pacman = "C";
+    let pacmanLocation = game.indexOf("C");
+    if (pacmanLocation == -1) {
+        pacmanLocation = game.indexOf("C.");
+    }
+    
+    if (pacmanLocation - 1 != 0) {
+        game[pacmanLocation] = "";
+        pacman += game[pacmanLocation - 1];
+        game[pacmanLocation - 1] = pacman;
+        pacman = "C";
+    }
+
+    console.log(game);
+
+}
+
+function moveRight(game) {
+    let pacman = "C";
+    let pacmanLocation = game.indexOf("C");
+    if (pacmanLocation == -1) {
+        pacmanLocation = game.indexOf("C.");
+    }
+    
+    if (pacmanLocation + 1 != game.length) {
+        game[pacmanLocation] = "";
+        pacman += game[pacmanLocation + 1];
+        game[pacmanLocation + 1] = pacman;
+        pacman = "C";
+    }
+
+    console.log(game);
+}
+
+let gameBoard = createGame(10);
+moveLeft(gameBoard);
+moveRight(gameBoard);
